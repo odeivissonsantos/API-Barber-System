d5cbb861caa2aa6fddbcb2fbdea1327d48bc472f
Deivisson Santos <deivissonsantos@hotmail.com>
2021-10-18 16:30:24 -0300

    implementado DTO para produtos

---
+++ src/main/java/io/github/odeivissonsantos/dtos/ProdutoDTO.java
@@ -0,0 +1,29 @@
+package io.github.odeivissonsantos.dtos;
+
+import io.github.odeivissonsantos.models.Produto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author: Deivisson Santos
+ * @version: 2.0
+ * @Email: deivissonsantos@hotmail.com
+ * @Contato: (71) 99188-8419 (WhatsApp)
+ */
+public class ProdutoDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private String codigoEan;
+
+    public ProdutoDTO(Produto obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.preco = obj.getPreco();
+        this.codigoEan = obj.getCodigoEan();
+    }
+}
